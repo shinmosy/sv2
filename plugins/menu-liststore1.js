f921b7a9632f9ac0d172472921d6a6bbcc88cfaf
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-05 01:29:17 +0700

    Update menu-liststore1.js

--- plugins/menu-liststore1.js
+++ plugins/menu-liststore1.js
@@ -101,8 +101,8 @@ conn.reply(global.nomorown + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text 
    m.reply('☑️ Pesan Telah terkirim ke Owner!\n_*Menunggu confirmasi Dari Owner...*_')
 }
 
-handler.help = ['premium']
-handler.tags = ['kabul' ]
+handler.help = ['premium', 'bokepmenu']
+handler.tags = ['premium' ]
 handler.command = /^(bokepmenu|menubokep)$/i
 
 export default handler
