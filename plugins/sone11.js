4c679de23a596ee2204f073088744f33957642df
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-05 04:19:37 +0700

    Create sone11.js

---
+++ plugins/sone11.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp4/7.mp4')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.command = ['bokep11']
+handler.help = ['bokep11']
+export default handler
