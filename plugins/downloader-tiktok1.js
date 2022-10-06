636a0ff2c94172455577c824cf1c0a65ea4950fa
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-06 21:41:09 +0700

    Update downloader-tiktok1.js

--- plugins/downloader-tiktok1.js
+++ plugins/downloader-tiktok1.js
@@ -1,28 +1,13 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
-
+import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    .catch(async _ => await tiktokdlv2(args[0]))
-        .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', 
-`
-⟐⟞⟚⟝⟮ *Usᴇʀɴᴀᴍᴇ:* ⟯⟞⟚⟝⟐
-┇⟣⟪ ${nickname} ⟫⟢
-▥ ━┉┄┄┈┈ ▢
-
-⟐⟞⟚⟝⟮ *Dᴇsᴄʀɪᴘᴛɪᴏɴ:* ⟯⟞⟚⟝⟐
-▥ ━┉┄┄┈┈ ▢
-${description}
-◈ ━┉┈┄┈┈ ►
-┇©AL BOT WHATSAPP〠
-◈ ━┉┈┄┈┈ ►
-`.trim(), m)
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
 }
-handler.help = ['tiktok', 't'].map(v => v + ' <url>')
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|k|t)$/i
+handler.command = /^(tik(tok2)?(dl)?)$/i
 
 export default handler
