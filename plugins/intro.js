346e6194a64c4de08a24d1db4d26a932895d6400
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-08 01:41:27 +0700

    fix-bot-v3

--- plugins/intro.js
+++ plugins/intro.js
@@ -37,11 +37,11 @@ let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•═════════════�
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, krtu, wm,'🫂SELAMAT DATANG','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'SELAMAT DATANG',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
