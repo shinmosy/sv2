3a133dfe96538b31fa4586a3e6d0fd7b5ade5f20
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-07 17:49:58 +0700

    fix-bot-v3

--- plugins/intro.js
+++ plugins/intro.js
@@ -1,41 +1,20 @@
-/*let handler = async m => {
+let handler = async m => {
+
 let krtu = `Kartu Intro`
 m.reply(`
-     *「 SELAMAT DATANG 」*
-*👑KABUL STORE || READY👑* 
- Jangan Lupa Baca Deskripsi Group
- Sebelum Bertindak 
-
-Untuk Melihat Semua Pricelist/pembayaran
-Ketik /list Semoga Betah Store Kami🙏
+0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Kartu Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+ |  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
 `.trim()) // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
-export default handler */
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
-let krtu = `
-    *「 SELAMAT DATANG 」*
-*👑KABUL STORE || READY👑* 
- Jangan Lupa Baca Deskripsi Group
- Sebelum Bertindak 
 
-Untuk Melihat Semua Pricelist/pembayaran
-Ketik /list Semoga Betah Store Kami🙏
-`
-let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
-let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, krtu, wm,'🛒STORE','.store', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/kabull_store",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'SELAMAT DATANG ',
-    body: wm,
-    thumbnail: thumb,
-    sourceUrl: sgc
-  }
-  } }) // Tambah sendiri kalo mau
-}
-handler.command = /^(intro)$/i
-export default handler
+module.exports = handler
