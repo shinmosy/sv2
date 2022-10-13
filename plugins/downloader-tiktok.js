a975e1cdceb60f47c982f4e48dec1ce3100ffce4
Ryumosy <114341726+shinmosy@users.noreply.github.com>
2022-10-14 02:04:20 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://vt.tiktok.com/ZSRb5AKWt`
     let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
